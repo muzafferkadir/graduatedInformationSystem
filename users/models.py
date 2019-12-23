4e5474022b4ac9cfbdc726b321a7d422b9b1e16d
Muzaffer Kadir YILMAz <muzafferkadiryilmaz@gmail.com>
2019-12-23 15:12:33 +0300

    login with student no

--- users/models.py
+++ users/models.py
@@ -6,11 +6,16 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class UserTable(AbstractUser):
     birthday = models.DateField(("Date"), default=date.today)
-    student_no = models.IntegerField(default = 0,validators=[MinValueValidator(10000), MaxValueValidator(999999)])
-    tc_no = models.IntegerField(default = 0,validators=[MinValueValidator(10000000000), MaxValueValidator(99999999999)])
+    student_no = models.IntegerField(default = 0,validators=[MinValueValidator(10000), MaxValueValidator(999999)],unique=True)
+    tc_no = models.IntegerField(default = 0,validators=[MinValueValidator(10000000000), MaxValueValidator(99999999999)],unique=True)
     department = models.CharField(max_length = 100)
     image =models.ImageField(upload_to='images/', null=True, blank=True)
     telephone = models.CharField(max_length = 20, blank = True, null = True)
 
+    USERNAME_FIELD = 'student_no'
+    
     def __str__(self):
         return self.username
+    class Meta:
+        unique_together = ('username', 'student_no','tc_no')
+    
