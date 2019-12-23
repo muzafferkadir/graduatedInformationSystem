73718c94a5c954a7cc0355de32d5d4c776fa4e9d
Muzaffer Kadir YILMAz <muzafferkadiryilmaz@gmail.com>
2019-12-24 00:19:45 +0300

    student's adverts list update and delete permission

--- adverts/views.py
+++ adverts/views.py
@@ -1,8 +1,9 @@
-from django.urls import reverse_lazy,reverse
+from django.urls import reverse_lazy,reverse,path
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from adverts.models import Advertisement
+from django.http import  HttpResponseRedirect
 
 class AdvertCreate(CreateView):
     model = Advertisement
@@ -20,15 +21,30 @@ class AdvertUpdate(UpdateView):
     template_name = 'updateAdvert.html'
     success_url = reverse_lazy('home')
 
+    def get(self, request, *args, **kwargs):#sadece kendi objelerini düzenleyebilir
+        self.object = self.get_object()
+        if self.object.employer.student_no != request.user.student_no:
+            return HttpResponseRedirect(reverse('detail-advert', kwargs={'pk': self.object.pk}))
+        return super().get(request, *args, **kwargs)       
+
 class AdvertDelete(DeleteView):
     model = Advertisement
     template_name = 'deleteAdvert.html'
     success_url = reverse_lazy('home')
+    
+    def get(self, request, *args, **kwargs):#sadece kendi objelerini düzenleyebilir
+        self.object = self.get_object()
+        if self.object.employer.student_no != request.user.student_no:
+            return HttpResponseRedirect(reverse('detail-advert', kwargs={'pk': self.object.pk}))
+        return super().get(request, *args, **kwargs)  
 
 class AdvertList(ListView):
     model = Advertisement
     paginate_by = 3  # if pagination is desired
     template_name = "profile.html"
+
+    def get_queryset(self):
+        return Advertisement.objects.filter(employer=self.request.user)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -38,6 +54,7 @@ class AdvertList(ListView):
 class AdvertDetail(DetailView):
     model = Advertisement
     template_name = "detailAdvert.html"
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
